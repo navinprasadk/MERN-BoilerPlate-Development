5ccc67adaff5f76cd659c57ad3096366ad9b45e7
navinprasadk <navinprasad0808@gmail.com>
2017-12-01 14:49:06 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import printMe from "./print.js";
+ async function getComponent() {
+var element = document.createElement('div');
+   const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   return element;
+}
+getComponent().then(component => {
+   document.body.appendChild(component);
+ })
